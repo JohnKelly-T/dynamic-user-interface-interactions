b594dee3613f2868a021047f718aa296b66a3ff9
john-kelly <johnkellytemena@gmail.com>
2025-04-10 19:15:34 +0800

    Setup dropdowns

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import { setupDropdown } from "@johnkelly-t/dropdown-menu";
 import "./styles/reset.css";
 import "./styles/global.css";
+
+
+let dropdowns = document.querySelectorAll('.dropdown-trigger');
+
+setupDropdown(dropdowns[0], "hover");
+setupDropdown(dropdowns[1], "hover", "center");
+setupDropdown(dropdowns[2], "hover", "right");
